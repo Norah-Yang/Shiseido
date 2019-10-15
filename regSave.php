5a087c2d70087b51a737eb31182f801f70c326fb
Ashley <824074642@qq.com>
2019-10-15 21:56:01 +0800

    15日晚，数据库已连接

--- regSave.php
+++ regSave.php
@@ -3,6 +3,7 @@
     //1、接收前端的数据
 
     $userTel = $_POST['userTel'];
+    $userId = $_POST['userId'];
     $userpass = $_POST['userpass'];
 
     //2、处理
@@ -14,19 +15,19 @@
         echo("数据库出错".mysql_error());
     }else{
         //2)、选择库（选择目的地）
-        mysql_select_db("sheseidodb",$conn);
+        mysql_select_db("shiseidodb",$conn);
 
         //3)、执行SQL语句（数据传输）
         //3.1)
-        $sqlstr="select * from vip where userTel='$userTel'";//查询该手机号在数据库中有没有。 
+        $sqlstr="select * from vip where userTel='$userTel'";//查询该用户名在数据库中有没有。 
         $result = mysql_query($sqlstr,$conn);
         $rows = mysql_num_rows($result);//获得结果的行数
         if($rows>0){
             //4)、关闭数据库
             mysql_close($conn);
-            echo "-1";//手机号被使用
+            echo "-1";//用户名被使用
         }else{
-            $sqlstr="insert into vip(userTel,userpass) values('$userTel','$userpass')";
+            $sqlstr="insert into vip(userTel,userId,userpass) values('$userTel','$userId','$userpass')";
             $result = mysql_query( $sqlstr,$conn);
             //4)、关闭数据库
             mysql_close($conn);
